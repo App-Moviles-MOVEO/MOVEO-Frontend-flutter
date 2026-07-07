c684a78776300d7eb55b8d908a8036d49976c81f
Alexiz248 <alexisfyd@gmail.com>
2026-07-06 19:30:38 -0500

    Change build directory path for the project

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,13 @@
+// Configura los repositorios de dependencias para TODOS los módulos del proyecto (raíz y submódulos)
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
-
+// Cambia la ruta del directorio de compilación ("build") del proyecto raíz.
+// Por defecto se crea dentro de /android/build, pero aquí se mueve dos niveles arriba (../../build),
+// quedando en la raíz general del proyecto Flutter. Esto previene errores de rutas demasiado largas en Windows.
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -18,7 +21,7 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-
+// Registra la tarea "clean" (limpieza) usando el tipo estándar 'Delete' de Gradle
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
